652233dcd3904c06fd38b06e5675b62eff3e6ebd
Melvin <mel000vin@yahoo.com>
2019-02-07 17:49:06 +0800

    Topic 40 - Added docker-compose.yml to test out go-cd

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 run_in_docker() {
     echo $1
 #   docker run hello-world
-    docker run --name $1_mysql -e MYSQL_ROOT_PASSWORD=password MYSQL_DATABASE=bookstoredatabase -d mysql:latest -p 
+    docker run --name $1_mysql -e MYSQL_ROOT_PASSWORD=password MYSQL_DATABASE=bookstoredatabase -d mysql:latest -p
     3306:3306
 
 }
