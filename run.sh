37c1a255f05392ff21e93d0a38009b0b1d31ad53
Melvin <mel000vin@yahoo.com>
2019-02-08 13:20:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,13 @@
 run_in_docker() {
     echo build is $@
 #   docker run hello-world
-    docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=bookstoredatabase mysql:5.7
+#   docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=bookstoredatabase mysql:5.7
+    containerid=`docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=bookstoredatabase mysql:5.7`
+    echo container id is $containerid
+
+
+    ipaddress=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $containerid`
+    echo ip address is $ipaddress
 }
 
 run_in_docker
