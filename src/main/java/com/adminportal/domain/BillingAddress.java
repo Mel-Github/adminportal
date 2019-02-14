10ebfbabdf42a95454b224b9e9641cdcc2ef2039
Melvin <mel000vin@yahoo.com>
2019-02-14 21:46:04 +0800

    Topic 54

---
+++ src/main/java/com/adminportal/domain/BillingAddress.java
@@ -0,0 +1,93 @@
+package com.adminportal.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class BillingAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String BillingAddressName;
+    private String BillingAddressStreet1;
+    private String BillingAddressStreet2;
+    private String BillingAddressCity;
+    private String BillingAddressState;
+    private String BillingAddressCountry;
+    private String BillingAddressZipCode;
+
+    @OneToOne
+    private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBillingAddressName() {
+        return BillingAddressName;
+    }
+
+    public void setBillingAddressName(String BillingAddressName) {
+        this.BillingAddressName = BillingAddressName;
+    }
+
+    public String getBillingAddressStreet1() {
+        return BillingAddressStreet1;
+    }
+
+    public void setBillingAddressStreet1(String BillingAddressStreet1) {
+        this.BillingAddressStreet1 = BillingAddressStreet1;
+    }
+
+    public String getBillingAddressStreet2() {
+        return BillingAddressStreet2;
+    }
+
+    public void setBillingAddressStreet2(String BillingAddressStreet2) {
+        this.BillingAddressStreet2 = BillingAddressStreet2;
+    }
+
+    public String getBillingAddressCity() {
+        return BillingAddressCity;
+    }
+
+    public void setBillingAddressCity(String BillingAddressCity) {
+        this.BillingAddressCity = BillingAddressCity;
+    }
+
+    public String getBillingAddressState() {
+        return BillingAddressState;
+    }
+
+    public void setBillingAddressState(String BillingAddressState) {
+        this.BillingAddressState = BillingAddressState;
+    }
+
+    public String getBillingAddressCountry() {
+        return BillingAddressCountry;
+    }
+
+    public void setBillingAddressCountry(String BillingAddressCountry) {
+        this.BillingAddressCountry = BillingAddressCountry;
+    }
+
+    public String getBillingAddressZipCode() {
+        return BillingAddressZipCode;
+    }
+
+    public void setBillingAddressZipCode(String BillingAddressZipCode) {
+        this.BillingAddressZipCode = BillingAddressZipCode;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
